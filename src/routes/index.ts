7db2b7799d618c5d4a36ff9efe4450b6e7516e5f
nara <parknara0213@gmail.com>
2024-12-17 21:50:19 +0900

    Feat: [추가] 라우터 팩토리 함수 생성하기
    Refs: #10

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+// express.Router() 인스턴스를 생성합니다.
+const router = express.Router();
+
+// 이 함수는 새로운 express.Router() 인스턴스를 반환합니다.
+// 반환된 라우터는 다른 모듈에서 사용될 수 있습니다.
+export default (): express.Router => {
+  // 사용법 : authRouter(router)
+  return router; // 라우터 인스턴스를 반환
+};
