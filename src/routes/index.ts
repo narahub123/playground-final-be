d52b9e05b817677c888fc16940767659d7afff45
nara <parknara0213@gmail.com>
2024-12-18 01:25:12 +0900

    Feat: [추가] user router 생성
    Refs: #20

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import userRouter from "./user.router";
 
 // express.Router() 인스턴스를 생성합니다.
 const router = express.Router();
@@ -6,6 +7,6 @@ const router = express.Router();
 // 이 함수는 새로운 express.Router() 인스턴스를 반환합니다.
 // 반환된 라우터는 다른 모듈에서 사용될 수 있습니다.
 export default (): express.Router => {
-  // 사용법 : authRouter(router)
+  userRouter(router);
   return router; // 라우터 인스턴스를 반환
 };
