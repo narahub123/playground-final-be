d1f610cfeb6b3b317a57a244e7b60b0decaecb72
nara <parknara0213@gmail.com>
2024-12-18 10:13:07 +0900

    Feat: [추가] getUserByUserId
    Ref: #20

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,5 +1,6 @@
+import { getUserByUserId } from "@controllers";
 import { Router } from "express";
 
 export default (router: Router) => {
-    
-}
+  router.get("getUserByUserId", getUserByUserId); // userId로 유저 정보 얻기
+};
