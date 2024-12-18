4539e5233a52549f9db057362ebf76195febf989
nara <parknara0213@gmail.com>
2024-12-18 10:46:30 +0900

    Feat: [추가] getUserByEmail, getUserByPhone 추가
    Refs: #20

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,6 +1,8 @@
-import { getUserByUserId } from "@controllers";
+import { getUserByEmail, getUserByPhone, getUserByUserId } from "@controllers";
 import { Router } from "express";
 
 export default (router: Router) => {
   router.get("getUserByUserId", getUserByUserId); // userId로 유저 정보 얻기
+  router.get("getUserByEmail", getUserByEmail); // 이메일로 유저 정보 얻기
+  router.get("getUserByPhone", getUserByPhone); // 전화번호로 유저 정보 얻기
 };
