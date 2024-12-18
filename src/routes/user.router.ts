3467b0cb842b042ba84f074c817f2ac33ee31b63
nara <parknara0213@gmail.com>
2024-12-18 11:50:35 +0900

    Fix: [수정] user router 경로 수정
    Refs: #20

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -2,7 +2,7 @@ import { getUserByEmail, getUserByPhone, getUserByUserId } from "@controllers";
 import { Router } from "express";
 
 export default (router: Router) => {
-  router.get("getUserByUserId", getUserByUserId); // userId로 유저 정보 얻기
-  router.get("getUserByEmail", getUserByEmail); // 이메일로 유저 정보 얻기
-  router.get("getUserByPhone", getUserByPhone); // 전화번호로 유저 정보 얻기
+  router.get("/user/getUserByUserId:userId", getUserByUserId); // userId로 유저 정보 얻기
+  router.get("/user/getUserByEmail:email", getUserByEmail); // 이메일로 유저 정보 얻기
+  router.get("/user/getUserByPhone:phone", getUserByPhone); // 전화번호로 유저 정보 얻기
 };
