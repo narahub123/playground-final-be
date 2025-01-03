dd858d9e8d10dcad15168555ba329828580ba995
nara <parknara0213@gmail.com>
2025-01-03 12:24:20 +0900

    Feat: [추가] signin 라우터 생성
    Refs: #23

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userRouter from "./user.router";
+import signinRouter from "./signin.router";
 
 // express.Router() 인스턴스를 생성합니다.
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 // 반환된 라우터는 다른 모듈에서 사용될 수 있습니다.
 export default (): express.Router => {
   userRouter(router);
+  signinRouter(router);
   return router; // 라우터 인스턴스를 반환
 };
