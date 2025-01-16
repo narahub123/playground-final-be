237dc7575c073720f97c9cfc2a5c5ffa3941258e
nara <parknara0213@gmail.com>
2025-01-16 17:04:17 +0900

    Fix: [수정] uploadImages 타입 수정
    Refs: #27

--- src/utils/uploadImages.ts
+++ src/utils/uploadImages.ts
@@ -1,11 +1,11 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 
 const uploadImages = async (images: string[]) => {
-  const results = [];
+  const results: UploadApiResponse[] = [];
   for (const image of images) {
     const result = await cloudinary.uploader.upload(image);
 
-    results.push(result.secure_url);
+    results.push(result);
   }
 
   return results;
