7fd5dcc1e0c3529cae2e8b63250197b5a1cf5bb5
nara <parknara0213@gmail.com>
2025-01-16 13:38:48 +0900

    Fix: [수정] location 타입 수정, gender required 삭제
    Refs: #14

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,13 @@
-const USERID_MAX = 15; // 유저 아이디 최소
-const USERID_MIN = 1; // 유저 아이디 최대
-const USERNAME_MAX = 20; // 유저 이름 최소
-const USERNAME_MIN = 4; // 유저 이름 최대
+const USERID_MAX = 30; // 유저 아이디 최소
+const USERID_MIN = 4; // 유저 아이디 최대
+const USERNAME_MAX = 30; // 유저 이름 최소
+const USERNAME_MIN = 1; // 유저 이름 최대
 const NUMOFBIRTH = 8; // 생일 숫자 개수
 const COUNTRY_DEFAULT = "ko"; // 국가
 const LANGUAGE_DEFAULT = "ko-KR"; // 언어
 const INTRO_MAX = 150; // 자기 소개 최대
+const EMAIL_SPECIAL_CHARACTERS =
+  "\\!\\#\\$\\%\\&\\'*\\+\\/\\=?\\^_\\`\\{\\|\\}\\~";
 
 export {
   COUNTRY_DEFAULT,
@@ -16,4 +18,5 @@ export {
   USERNAME_MAX,
   USERNAME_MIN,
   NUMOFBIRTH,
+  EMAIL_SPECIAL_CHARACTERS,
 };
--- src/data/regExp.ts
+++ src/data/regExp.ts
@@ -1,4 +1,5 @@
 import {
+  EMAIL_SPECIAL_CHARACTERS,
   NUMOFBIRTH,
   USERID_MAX,
   USERID_MIN,
@@ -8,7 +9,9 @@ import {
 
 const userIdRegExp = new RegExp(`^[a-z0-9_]{${USERID_MIN},${USERID_MAX}}$`);
 const usernameRegExp = new RegExp(`^.{${USERNAME_MIN},${USERNAME_MAX}}$`);
-const emailRegExp = new RegExp(`^\S+@\S+\.\S+$`);
+const emailRegExp = new RegExp(
+  `^[0-9a-zA-Z${EMAIL_SPECIAL_CHARACTERS}]+([.][0-9a-zA-Z${EMAIL_SPECIAL_CHARACTERS}]+)*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?$`
+);
 const birthRegExp = new RegExp(`^[0-9]{${NUMOFBIRTH}}$`);
 const phoneRegExp = new RegExp(
   `^(?:\\+?\\d{1,3}[-\\s]?)?(?:\\(?\\d{1,4}\\)?[-\\s]?)?\\d{1,4}[-\\s]?\\d{1,4}[-\\s]?\\d{1,4}$`
