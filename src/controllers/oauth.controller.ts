55dbae546bfa44a17714c07b595b059ecce18ec9
nara <parknara0213@gmail.com>
2025-01-19 14:39:37 +0900

    Feat: [추가] 기존 가입자 여부 확인 후 비 가입자인 경우 쿠키로 사용자 정보 전송
    Refs: #31

--- src/controllers/oauth.controller.ts
+++ src/controllers/oauth.controller.ts
@@ -4,6 +4,7 @@ import { getOauthAccessToken, getOauthUserInfo } from "@utils";
 import { OauthType, UserData } from "@types";
 import { BASE_URL } from "@constants";
 import { BadRequestError } from "@errors";
+import { checkEmailDuplicate } from "@services";
 
 const oauthCallback = asyncWrapper(
   "oauthCallback",
@@ -22,7 +23,25 @@ const oauthCallback = asyncWrapper(
         type
       );
 
-      console.log(userData);
+      if (!userData) {
+        throw new Error("유저 정보 취득 실패");
+      }
+
+      // 기존 유저 여부 확인하기
+      const user = await checkEmailDuplicate(userData.email);
+
+      // 기존 유저 정보가 존재한다면 로그인 과정으로
+      if (user) {
+      } else {
+        // 기존 유저 정보가 존재하지 않는다면 회원 가입 과정으로
+        // 쿠키로 사용자 정보를 전송
+        res.cookie("oauth_user", JSON.stringify(userData), {
+          maxAge: 1000 * 60 * 60,
+        });
+
+        // 리다이렉트 주소를 oauth/callback으로 함
+        res.redirect(`${BASE_URL}/oauth/callback`);
+      }
     }
   }
 );
