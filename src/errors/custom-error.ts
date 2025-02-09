8d5dd09116657d35430111576f4d03541d182413
nara <parknara0213@gmail.com>
2025-02-10 06:47:36 +0900

    Fix: [수정] errorCode 추가
    Refs: #8

--- src/errors/custom-error.ts
+++ src/errors/custom-error.ts
@@ -1,15 +1,18 @@
 class CustomAPIError extends Error {
   statusCode: number;
   statusText: string;
+  errorCode: string;
 
   constructor(
     message: string,
     statusCode: number = 500,
-    statusText: string = "내부 에러"
+    statusText: string = "Internal Error",
+    errorCode: string = "INTERNAL_ERROR"
   ) {
     super(message);
     this.statusCode = statusCode;
     this.statusText = statusText;
+    this.errorCode = errorCode;
     this.name = this.constructor.name;
     Error.captureStackTrace(this, this.constructor);
   }
