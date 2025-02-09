3873503f5904f09f1c7fd1e2dbb9d29f0a0334b4
nara <parknara0213@gmail.com>
2025-02-09 15:34:16 +0900

    Fix: [수정] signup api 수정

--- src/routes/signup.router.ts
+++ src/routes/signup.router.ts
@@ -7,9 +7,12 @@ import {
 
 export default (router: Router) => {
   // 이메일 중복 검사
-  router.post("/signup/checkEmailDuplication", checkEmailDuplicationInSignup);
+  router.post("/signup/check-email-duplication", checkEmailDuplicationInSignup);
   // 사용자 아이디 중복 검사
-  router.post("/signup/checkUserIdDuplication", checkUserIdDuplicationInSignup);
+  router.post(
+    "/signup/check-userid-duplication",
+    checkUserIdDuplicationInSignup
+  );
   // 유저 정보 등록
   router.post("/signup/signupUser", signupUser);
 };
